fbcec4ff9f46adac79b439cc0833da256e363dbf
fd8666 <154691237+fd8666@users.noreply.github.com>
2024-11-02 16:29:14 +0100

    Add files via upload

---
+++ WebAppLab/src/main/java/it/unical/inf/webappes1/WebAppEs3Application.java
@@ -0,0 +1,13 @@
+package it.unical.inf.webappes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppEs3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppEs3Application.class, args);
+    }
+
+}
